3be53440ead95eef59692a44b375562948a50813
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-05-15 00:35:53 +0100

    Add state for loading

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,12 +9,14 @@ import classes from "./index.module.css";
 export default function Home() {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const weatherAPI = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
   const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherAPI}&units=metric`;
 
   const fetchWeatherData = async (e) => {
     e.preventDefault();
+    setLoading(true);
     try {
       const response = await axios.get(weatherURL);
       setWeather(response.data);
@@ -22,34 +24,40 @@ export default function Home() {
     } catch (error) {
       console.log(error);
     }
+    setLoading(false);
   };
-  return (
-    <Fragment>
-      <Head>
-        <title>HelioStorm</title>
-        <meta name="description" content="Your weather app for everyday" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <div className={classes.main}>
-        <div className={classes.weather}>
-          <div className={classes.input}>
-            <h2>GET THE WEATHER</h2>
-            <div>
-              <input
-                type="text"
-                value={city}
-                placeholder="SEARCH CITY"
-                onChange={(e) => setCity(e.target.value)}
-              />
-              <button onClick={fetchWeatherData}>
-                <AiOutlineSearch />
-              </button>
+
+  if (loading) {
+    return <h1>Loading ...</h1>;
+  } else {
+    return (
+      <Fragment>
+        <Head>
+          <title>HelioStorm</title>
+          <meta name="description" content="Your weather app for everyday" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <div className={classes.main}>
+          <div className={classes.weather}>
+            <div className={classes.input}>
+              <h2>GET THE WEATHER</h2>
+              <div>
+                <input
+                  type="text"
+                  value={city}
+                  placeholder="SEARCH CITY"
+                  onChange={(e) => setCity(e.target.value)}
+                />
+                <button onClick={fetchWeatherData}>
+                  <AiOutlineSearch />
+                </button>
+              </div>
             </div>
+            {/* Weather content */}
+            {weather && <Weather data={weather} />}
           </div>
-          {/* Weather content */}
-          {weather && <Weather data={weather} />}
         </div>
-      </div>
-    </Fragment>
-  );
+      </Fragment>
+    );
+  }
 }
